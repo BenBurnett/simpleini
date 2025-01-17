25a15132cf26d364a3a029f117eeb54b8817d7e5
Ben Burnett <burnett.73@gmail.com>
2025-01-16 22:50:34 -0500

    Improve documentation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Simple INI
 
-Simple INI is a Go library for parsing INI files.
+Simple INI is a Go library for parsing INI files using reflection. It provides a simple and flexible way to map INI file keys to Go struct fields.
 
 [![GoDoc](https://godoc.org/github.com/BenBurnett/simpleini?status.svg)](https://godoc.org/github.com/BenBurnett/simpleini)
 [![Go Report Card](https://goreportcard.com/badge/github.com/BenBurnett/simpleini)](https://goreportcard.com/report/github.com/BenBurnett/simpleini)
@@ -14,14 +14,26 @@ Simple INI is a Go library for parsing INI files.
 
 ---
 
-## Features
+## Table of Contents
 
-- Support for sectionless blocks, section blocks, and sub-section blocks
-- Support for multiple data types: int, uint, float, bool, and string
-- Handles pointers to structs and basic types
-- Supports custom types that implement the `encoding.TextUnmarshaler` interface
-- Implicit name mapping from snake_case to PascalCase
-- Ability to override implicit name mapping with struct tags
+- [Installation](#installation)
+- [Example](#example)
+- [Features](#features)
+  - [Implicit Key Name Mapping](#implicit-key-name-mapping)
+  - [Overriding Implicit Name Mapping](#overriding-implicit-name-mapping)
+  - [Default Values](#default-values)
+  - [Comments](#comments)
+  - [Custom Delimiter](#custom-delimiter)
+  - [Sections and Subsections](#sections-and-subsections)
+  - [Custom Types](#custom-types)
+  - [Multiline](#multiline)
+  - [Slices](#slices)
+  - [Environment Variable Expansion](#environment-variable-expansion)
+  - [Include Directive](#include-directive)
+- [Usage](#usage)
+- [Error Handling](#error-handling)
+- [Contributing](#contributing)
+- [License](#license)
 
 ## Installation
 
@@ -33,21 +45,360 @@ go get github.com/BenBurnett/simpleini
 
 ## Example
 
-An example usage of Simple INI can be found in the `examples` folder. This example demonstrates how to define a struct with `ini` tags and parse an INI file into that struct.
+An example usage of Simple INI can be found in the `example` folder. This example demonstrates how to define a struct with `ini` tags and parse an INI file into that struct.
 
-### Implicit Name Mapping
+## Features
+
+### Implicit Key Name Mapping
 
 By default, Simple INI maps snake_case keys in the INI file to PascalCase field names in the Go struct. For example, the key `app_name` in the INI file will map to the field `AppName` in the Go struct.
+
+```ini
+app_name = MyApp
+app_version = 1.0.0
+```
+
+```go
+type AppConfig struct {
+	AppName    string
+	AppVersion string
+}
+```
 
 ### Overriding Implicit Name Mapping
 
 You can override the implicit name mapping by using struct tags. For example, if you want the key `ip_address` in the INI file to map to the field `IP` in the Go struct, you can use the following struct tag:
 
+```ini
+ip_address = 192.168.1.1
+server_name = MyServer
+maximum_connections = 100
+```
+
 ```go
 type ServerConfig struct {
-	IP *net.IP `ini:"ip_address"`
+	IP             *net.IP `ini:"ip_address"`
+	ServerName     string
+	MaxConnections int     `ini:"maximum_connections"`
 }
 ```
+
+### Default Values
+
+You can specify default values for fields using the `default` struct tag. These values will be used if the corresponding key is not present in the INI file.
+
+```ini
+app_name = MyApp
+```
+
+```go
+type AppConfig struct {
+	AppName    string `default:"DefaultApp"`
+	AppVersion string `default:"1.0.0"`
+}
+```
+
+### Comments
+
+Simple INI supports comments. Lines starting with `;` or `#` are treated as comments and ignored.
+
+```ini
+; This is a comment
+# This is also a comment
+app_name = MyApp
+```
+
+### Custom Delimiter
+
+You can specify a custom delimiter for key-value pairs in the INI file. By default, the delimiter is `=`.
+
+```ini
+app_name: MyApp
+app_version: 1.0.0
+```
+
+```go
+type AppConfig struct {
+	AppName    string
+	AppVersion string
+}
+
+var config AppConfig
+err := simpleini.ParseWithDelimiter(strings.NewReader(iniData), &config, ":")
+if err != nil {
+	log.Fatal(err)
+}
+fmt.Println(config.AppName)    // Output: MyApp
+fmt.Println(config.AppVersion) // Output: 1.0.0
+```
+
+### Sections and Subsections
+
+Simple INI supports sections and subsections in the INI file. Sections are defined using square brackets, and subsections can be defined using dot notation.
+
+```ini
+app_name = MyApp
+app_version = 1.0.0
+
+[database]
+host = localhost
+port = 5432
+
+[server]
+ip_address = 192.168.1.1
+
+[server.logging]
+level = debug
+
+[server.logging.file]
+path = /var/log/myapp.log
+```
+
+```go
+type DatabaseConfig struct {
+	Host string
+	Port int
+}
+
+type FileLoggingConfig struct {
+	Path string
+}
+
+type LoggingConfig struct {
+	Level string
+	File  FileLoggingConfig
+}
+
+type ServerConfig struct {
+	IPAddress string
+	Logging   LoggingConfig
+}
+
+type Config struct {
+	AppName    string
+	AppVersion string
+	Database   DatabaseConfig
+	Server     ServerConfig
+}
+```
+
+### Custom Types
+
+Simple INI supports custom types that implement the `encoding.TextUnmarshaler` interface. This allows you to define custom parsing logic for specific fields.
+
+```ini
+date = 2023-10-01
+```
+
+```go
+type CustomDate struct {
+	time.Time
+}
+
+func (d *CustomDate) UnmarshalText(text []byte) error {
+	parsedTime, err := time.Parse("2006-01-02", string(text))
+	if err != nil {
+		return err
+	}
+	d.Time = parsedTime
+	return nil
+}
+
+type Config struct {
+	Date CustomDate
+}
+```
+
+### Multiline
+
+Simple INI supports multiline values for strings. A multiline value continues when the next line starts with a space or tab.
+
+```ini
+description = This is a long description
+              that spans multiple lines.
+```
+
+```go
+type Config struct {
+	Description string
+}
+```
+
+### Slices
+
+Simple INI supports parsing slices from multiline values in the INI file. A multiline value continues when the next line starts with a space or tab.
+
+```ini
+servers = server1
+          server2
+          server3
+```
+
+```go
+type Config struct {
+	Servers []string `ini:"servers"`
+}
+```
+
+**Note:** A slice of custom types will call the `encoding.TextUnmarshaler` for each value. A single custom type will call it with the entire multiline value and can parse it in any way.
+
+### Environment Variable Expansion
+
+Simple INI supports expanding environment variables in values. Environment variables are referenced using the `${VAR_NAME}` syntax.
+
+```ini
+path = ${HOME}/myapp/config
+```
+
+```go
+type Config struct {
+	Path string
+}
+```
+
+### Include Directive
+
+Simple INI supports including other INI files using the `!include` directive. The included file's content will be parsed as if it were part of the original file.
+
+```ini
+other_field = i was included
+```
+```ini
+!include other_config.ini
+
+app_name = MyApp
+```
+
+```go
+type AppConfig struct {
+	AppName string
+	OtherField string
+}
+```
+
+## Usage
+
+This example demonstrates how to use several features of Simple INI, including implicit key name mapping, overriding implicit name mapping, default values, custom types, and environment variable expansion.
+
+```ini
+app_name = MyApp
+app_version = 1.0.0
+date = 2023-10-01
+
+[database]
+host = localhost
+port = 5432
+
+[server]
+ip_address = 192.168.1.1
+server_name = MyServer
+maximum_connections = 100
+
+[server.logging]
+level = debug
+
+[server.logging.file]
+path = ${HOME}/myapp/logs/app.log
+```
+
+```go
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/BenBurnett/simpleini"
+)
+
+type CustomDate struct {
+	time.Time
+}
+
+func (d *CustomDate) UnmarshalText(text []byte) error {
+	parsedTime, err := time.Parse("2006-01-02", string(text))
+	if err != nil {
+		return err
+	}
+	d.Time = parsedTime
+	return nil
+}
+
+type DatabaseConfig struct {
+	Host string
+	Port int
+}
+
+type FileLoggingConfig struct {
+	Path string
+}
+
+type LoggingConfig struct {
+	Level string
+	File  FileLoggingConfig
+}
+
+type ServerConfig struct {
+	IPAddress       string `ini:"ip_address"`
+	ServerName      string `ini:"server_name"`
+	MaxConnections  int    `ini:"maximum_connections"`
+	Logging         LoggingConfig
+}
+
+type AppConfig struct {
+	AppName    string
+	AppVersion string
+	Date       CustomDate
+	Database   DatabaseConfig
+	Server     ServerConfig
+}
+
+func main() {
+	file, err := os.Open("config.ini")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	os.Setenv("HOME", "/home/user")
+
+	var config AppConfig
+	err = simpleini.Parse(file, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("AppName:", config.AppName)                   // Output: MyApp
+	fmt.Println("AppVersion:", config.AppVersion)             // Output: 1.0.0
+	fmt.Println("Database Host:", config.Database.Host)       // Output: localhost
+	fmt.Println("Database Port:", config.Database.Port)       // Output: 5432
+	fmt.Println("Server IP Address:", config.Server.IPAddress) // Output: 192.168.1.1
+	fmt.Println("Server Name:", config.Server.ServerName)     // Output: MyServer
+	fmt.Println("Max Connections:", config.Server.MaxConnections) // Output: 100
+	fmt.Println("Logging Level:", config.Server.Logging.Level) // Output: debug
+	fmt.Println("Log File Path:", config.Server.Logging.File.Path) // Output: /home/user/myapp/logs/app.log
+	fmt.Println("Date:", config.Date.Format("2006-01-02"))    // Output: 2023-10-01
+}
+```
+
+## Error Handling
+
+Simple INI returns a slice of errors if any issues are encountered during parsing. You can handle these errors in your code to provide meaningful feedback to the user.
+
+```go
+errors := simpleini.Parse(file, &config)
+if len(errors) > 0 {
+	for _, err := range errors {
+		fmt.Println("Error:", err)
+	}
+}
+```
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request on GitHub.
 
 ## License
 
--- parser.go
+++ parser.go
@@ -18,7 +18,8 @@ import (
 // Cache for struct field mappings
 var fieldCache sync.Map
 
-// getFieldMap returns the field map for the given struct type
+// getFieldMap returns the field map for the given struct type.
+// It uses a cache to avoid recomputing the field map for the same type.
 func getFieldMap(t reflect.Type) (map[string]reflect.StructField, error) {
 	if fieldMap, found := fieldCache.Load(t); found {
 		return fieldMap.(map[string]reflect.StructField), nil
@@ -41,160 +42,34 @@ func getFieldMap(t reflect.Type) (map[string]reflect.StructField, error) {
 	return fieldMap, nil
 }
 
-// substituteEnvVars replaces placeholders with environment variable values
+// snakeToPascal converts a snake_case string to PascalCase.
+func snakeToPascal(s string) string {
+	var result strings.Builder
+	upperNext := true
+	for _, r := range s {
+		if r == '_' {
+			upperNext = true
+			continue
+		}
+
+		if upperNext {
+			result.WriteRune(unicode.ToUpper(r))
+			upperNext = false
+		} else {
+			result.WriteRune(r)
+		}
+	}
+	return result.String()
+}
+
+// substituteEnvVars replaces placeholders in the value with environment variable values.
 func substituteEnvVars(value string) string {
 	return os.Expand(value, func(key string) string {
 		return os.Getenv(key)
 	})
 }
 
-// handleIncludeDirective processes an include directive
-func handleIncludeDirective(line, basePath string, config interface{}, delimiter string, includedFiles map[string]bool, depth int) ([]error, bool) {
-	if strings.HasPrefix(line, "!include ") {
-		includeFile := strings.TrimSpace(line[len("!include "):])
-		if !filepath.IsAbs(includeFile) {
-			includeFile = filepath.Join(basePath, includeFile)
-		}
-		includeErrors := parseFile(includeFile, config, delimiter, includedFiles, depth)
-		return includeErrors, true
-	}
-	return nil, false
-}
-
-// parseFile reads and parses an INI file with support for include directives
-func parseFile(filename string, config interface{}, delimiter string, includedFiles map[string]bool, depth int) []error {
-	if depth > 10 {
-		return []error{fmt.Errorf("maximum include depth exceeded")}
-	}
-
-	if includedFiles[filename] {
-		return []error{fmt.Errorf("circular include detected: %s", filename)}
-	}
-	includedFiles[filename] = true
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return []error{fmt.Errorf("failed to open file: %w", err)}
-	}
-	defer file.Close()
-
-	basePath := filepath.Dir(filename)
-	return parseReader(file, config, delimiter, includedFiles, depth+1, basePath)
-}
-
-// processMultilineValue processes and sets a multiline value
-func processMultilineValue(config interface{}, section, key, value string, lineNumber int) error {
-	value = substituteEnvVars(value)
-	if err := setConfigValue(config, section, key, value); err != nil {
-		return fmt.Errorf("error at line %d: %w", lineNumber, err)
-	}
-	return nil
-}
-
-// processLine processes a single line from the INI file
-func processLine(line string, config interface{}, delimiter string, currentSection *string, currentKey *string, currentValue *string, inMultiline *bool, lineNumber int) error {
-	// Check for multiline continuation
-	if strings.HasPrefix(line, " ") || strings.HasPrefix(line, "\t") {
-		*inMultiline = true
-		*currentValue += "\n" + strings.TrimSpace(line)
-		return nil
-	}
-
-	// Process the previous multiline value
-	if *inMultiline {
-		if err := processMultilineValue(config, *currentSection, *currentKey, *currentValue, lineNumber); err != nil {
-			return err
-		}
-		*inMultiline = false
-	}
-
-	line = strings.TrimSpace(line)
-	if len(line) == 0 || line[0] == ';' || line[0] == '#' {
-		return nil
-	}
-
-	// Check if the line is a section header
-	if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
-		*currentSection = strings.ToLower(line[1 : len(line)-1])
-	} else {
-		// Check if the line is a key-value pair
-		if !strings.Contains(line, delimiter) {
-			return fmt.Errorf("invalid line format at line %d: %s", lineNumber, line)
-		}
-
-		// Split the line into key and value
-		keyValue := strings.SplitN(line, delimiter, 2)
-		*currentKey = strings.ToLower(strings.TrimSpace(keyValue[0]))
-		*currentValue = strings.TrimSpace(keyValue[1])
-		*currentValue = substituteEnvVars(*currentValue)
-
-		// Use reflection to set the value in the config struct
-		if err := setConfigValue(config, *currentSection, *currentKey, *currentValue); err != nil {
-			return fmt.Errorf("error at line %d: %w", lineNumber, err)
-		}
-	}
-
-	return nil
-}
-
-// parseReader parses the INI content from an io.Reader with support for include directives
-func parseReader(reader io.Reader, config interface{}, delimiter string, includedFiles map[string]bool, depth int, basePath string) []error {
-	var errors []error
-
-	// Set default values for all fields
-	if err := setDefaultValues(reflect.ValueOf(config).Elem()); err != nil {
-		errors = append(errors, err)
-	}
-
-	scanner := bufio.NewScanner(reader)
-	var currentSection, currentKey, currentValue string
-	var inMultiline bool
-	lineNumber := 0
-
-	// Read the file line by line
-	for scanner.Scan() {
-		lineNumber++
-		line := scanner.Text()
-
-		// Handle include directive
-		if includeErrors, handled := handleIncludeDirective(line, basePath, config, delimiter, includedFiles, depth); handled {
-			if includeErrors != nil {
-				errors = append(errors, includeErrors...)
-			}
-			continue
-		}
-
-		// Process the line
-		if err := processLine(line, config, delimiter, &currentSection, &currentKey, &currentValue, &inMultiline, lineNumber); err != nil {
-			errors = append(errors, err)
-		}
-	}
-
-	// Process any remaining multiline value
-	if inMultiline {
-		if err := processMultilineValue(config, currentSection, currentKey, currentValue, lineNumber); err != nil {
-			errors = append(errors, err)
-		}
-	}
-
-	if len(errors) > 0 {
-		return errors
-	}
-
-	return nil
-}
-
-// Parse parses the INI file content from an io.Reader and populates the config struct
-func Parse(reader io.Reader, config interface{}) []error {
-	return parseReader(reader, config, "=", make(map[string]bool), 0, "")
-}
-
-// ParseWithDelimiter parses the INI file content from an io.Reader with a custom delimiter and populates the config struct
-func ParseWithDelimiter(reader io.Reader, config interface{}, delimiter string) []error {
-	return parseReader(reader, config, delimiter, make(map[string]bool), 0, "")
-}
-
-// initializePointer initializes a pointer if it is nil
+// initializePointer initializes a pointer if it is nil and returns the dereferenced value.
 func initializePointer(v reflect.Value) reflect.Value {
 	if v.Kind() == reflect.Ptr {
 		if v.IsNil() {
@@ -205,94 +80,7 @@ func initializePointer(v reflect.Value) reflect.Value {
 	return v
 }
 
-// setDefaultValues sets the default values for all fields in the struct
-func setDefaultValues(v reflect.Value) error {
-	fieldMap, err := getFieldMap(v.Type())
-	if err != nil {
-		return err
-	}
-
-	for _, field := range fieldMap {
-		fieldValue := initializePointer(v.FieldByName(field.Name))
-		defaultValue := field.Tag.Get("default")
-		if defaultValue != "" {
-			if err := setFieldValue(fieldValue, defaultValue); err != nil {
-				return err
-			}
-		}
-
-		// Recursively set default values for nested structs
-		if fieldValue.Kind() == reflect.Struct {
-			if err := setDefaultValues(fieldValue); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-// setConfigValue sets the value of a field in the config struct
-func setConfigValue(config interface{}, section, key, value string) error {
-	// Check if the config is a pointer to a struct
-	v := reflect.ValueOf(config)
-	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
-		return errors.New("configuration must be a pointer to a struct")
-	}
-	v = v.Elem()
-
-	// If no section is specified, set the value in the root struct
-	if section == "" {
-		return setStructValue(v, key, value)
-	}
-
-	// Traverse the struct fields to find the section
-	sectionParts := strings.Split(section, ".")
-	for _, part := range sectionParts {
-		part = strings.ToLower(part)
-		// Find the field by tag or converted name
-		field := v.FieldByNameFunc(func(name string) bool {
-			field, ok := v.Type().FieldByName(name)
-			return ok && (strings.EqualFold(field.Tag.Get("ini"), part) || strings.EqualFold(snakeToPascal(part), name))
-		})
-
-		// If the field is not found, return an error
-		if !field.IsValid() {
-			return fmt.Errorf("no matching field found for section '%s'", section)
-		}
-
-		// Initialize the pointer if necessary
-		field = initializePointer(field)
-
-		// Check if the field is a struct
-		if field.Kind() != reflect.Struct {
-			return fmt.Errorf("field for section '%s' is not a struct", section)
-		}
-		v = field
-	}
-
-	return setStructValue(v, key, value)
-}
-
-// setStructValue sets the value of a field in the struct
-func setStructValue(v reflect.Value, key, value string) error {
-	fieldMap, err := getFieldMap(v.Type())
-	if err != nil {
-		return err
-	}
-
-	// Find the field by key
-	field, ok := fieldMap[key]
-	if !ok {
-		field, ok = fieldMap[snakeToPascal(key)]
-		if !ok {
-			return fmt.Errorf("no matching field found for key '%s'", key)
-		}
-	}
-
-	return setFieldValue(v.FieldByName(field.Name), value)
-}
-
-// setFieldValue sets the value of a field
+// setFieldValue sets the value of a field based on its type.
 func setFieldValue(fieldValue reflect.Value, value string) error {
 	// Initialize the pointer if necessary
 	fieldValue = initializePointer(fieldValue)
@@ -349,22 +137,235 @@ func setFieldValue(fieldValue reflect.Value, value string) error {
 	return nil
 }
 
-// snakeToPascal converts a snake_case string to PascalCase
-func snakeToPascal(s string) string {
-	var result strings.Builder
-	upperNext := true
-	for _, r := range s {
-		if r == '_' {
-			upperNext = true
+// setDefaultValues sets the default values for all fields in the struct.
+func setDefaultValues(v reflect.Value) error {
+	fieldMap, err := getFieldMap(v.Type())
+	if err != nil {
+		return err
+	}
+
+	for _, field := range fieldMap {
+		fieldValue := initializePointer(v.FieldByName(field.Name))
+		defaultValue := field.Tag.Get("default")
+		if defaultValue != "" {
+			if err := setFieldValue(fieldValue, defaultValue); err != nil {
+				return err
+			}
+		}
+
+		// Recursively set default values for nested structs
+		if fieldValue.Kind() == reflect.Struct {
+			if err := setDefaultValues(fieldValue); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// setStructValue sets the value of a field in the struct.
+func setStructValue(v reflect.Value, key, value string) error {
+	fieldMap, err := getFieldMap(v.Type())
+	if err != nil {
+		return err
+	}
+
+	// Find the field by key
+	field, ok := fieldMap[key]
+	if !ok {
+		field, ok = fieldMap[snakeToPascal(key)]
+		if !ok {
+			return fmt.Errorf("no matching field found for key '%s'", key)
+		}
+	}
+
+	return setFieldValue(v.FieldByName(field.Name), value)
+}
+
+// setConfigValue sets the value of a field in the config struct.
+func setConfigValue(config interface{}, section, key, value string) error {
+	// Check if the config is a pointer to a struct
+	v := reflect.ValueOf(config)
+	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
+		return errors.New("configuration must be a pointer to a struct")
+	}
+	v = v.Elem()
+
+	// If no section is specified, set the value in the root struct
+	if section == "" {
+		return setStructValue(v, key, value)
+	}
+
+	// Traverse the struct fields to find the section
+	sectionParts := strings.Split(section, ".")
+	for _, part := range sectionParts {
+		part = strings.ToLower(part)
+		// Find the field by tag or converted name
+		field := v.FieldByNameFunc(func(name string) bool {
+			field, ok := v.Type().FieldByName(name)
+			return ok && (strings.EqualFold(field.Tag.Get("ini"), part) || strings.EqualFold(snakeToPascal(part), name))
+		})
+
+		// If the field is not found, return an error
+		if !field.IsValid() {
+			return fmt.Errorf("no matching field found for section '%s'", section)
+		}
+
+		// Initialize the pointer if necessary
+		field = initializePointer(field)
+
+		// Check if the field is a struct
+		if field.Kind() != reflect.Struct {
+			return fmt.Errorf("field for section '%s' is not a struct", section)
+		}
+		v = field
+	}
+
+	return setStructValue(v, key, value)
+}
+
+// processMultilineValue processes and sets a multiline value.
+func processMultilineValue(config interface{}, section, key, value string, lineNumber int) error {
+	value = substituteEnvVars(value)
+	if err := setConfigValue(config, section, key, value); err != nil {
+		return fmt.Errorf("error at line %d: %w", lineNumber, err)
+	}
+	return nil
+}
+
+// processLine processes a single line from the INI file.
+func processLine(line string, config interface{}, delimiter string, currentSection *string, currentKey *string, currentValue *string, inMultiline *bool, lineNumber int) error {
+	// Check for multiline continuation
+	if strings.HasPrefix(line, " ") || strings.HasPrefix(line, "\t") {
+		*inMultiline = true
+		*currentValue += "\n" + strings.TrimSpace(line)
+		return nil
+	}
+
+	// Process the previous multiline value
+	if *inMultiline {
+		if err := processMultilineValue(config, *currentSection, *currentKey, *currentValue, lineNumber); err != nil {
+			return err
+		}
+		*inMultiline = false
+	}
+
+	line = strings.TrimSpace(line)
+	if len(line) == 0 || line[0] == ';' || line[0] == '#' {
+		return nil
+	}
+
+	// Check if the line is a section header
+	if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
+		*currentSection = strings.ToLower(line[1 : len(line)-1])
+	} else {
+		// Check if the line is a key-value pair
+		if !strings.Contains(line, delimiter) {
+			return fmt.Errorf("invalid line format at line %d: %s", lineNumber, line)
+		}
+
+		// Split the line into key and value
+		keyValue := strings.SplitN(line, delimiter, 2)
+		*currentKey = strings.ToLower(strings.TrimSpace(keyValue[0]))
+		*currentValue = strings.TrimSpace(keyValue[1])
+		*currentValue = substituteEnvVars(*currentValue)
+
+		// Use reflection to set the value in the config struct
+		if err := setConfigValue(config, *currentSection, *currentKey, *currentValue); err != nil {
+			return fmt.Errorf("error at line %d: %w", lineNumber, err)
+		}
+	}
+
+	return nil
+}
+
+// handleIncludeDirective processes an include directive.
+func handleIncludeDirective(line, basePath string, config interface{}, delimiter string, includedFiles map[string]bool, depth int) ([]error, bool) {
+	if strings.HasPrefix(line, "!include ") {
+		includeFile := strings.TrimSpace(line[len("!include "):])
+		if !filepath.IsAbs(includeFile) {
+			includeFile = filepath.Join(basePath, includeFile)
+		}
+		includeErrors := parseFile(includeFile, config, delimiter, includedFiles, depth)
+		return includeErrors, true
+	}
+	return nil, false
+}
+
+// parseReader parses the INI content from an io.Reader with support for include directives.
+func parseReader(reader io.Reader, config interface{}, delimiter string, includedFiles map[string]bool, depth int, basePath string) []error {
+	var errors []error
+
+	// Set default values for all fields
+	if err := setDefaultValues(reflect.ValueOf(config).Elem()); err != nil {
+		errors = append(errors, err)
+	}
+
+	scanner := bufio.NewScanner(reader)
+	var currentSection, currentKey, currentValue string
+	var inMultiline bool
+	lineNumber := 0
+
+	// Read the file line by line
+	for scanner.Scan() {
+		lineNumber++
+		line := scanner.Text()
+
+		// Handle include directive
+		if includeErrors, handled := handleIncludeDirective(line, basePath, config, delimiter, includedFiles, depth); handled {
+			if includeErrors != nil {
+				errors = append(errors, includeErrors...)
+			}
 			continue
 		}
 
-		if upperNext {
-			result.WriteRune(unicode.ToUpper(r))
-			upperNext = false
-		} else {
-			result.WriteRune(r)
+		// Process the line
+		if err := processLine(line, config, delimiter, &currentSection, &currentKey, &currentValue, &inMultiline, lineNumber); err != nil {
+			errors = append(errors, err)
 		}
 	}
-	return result.String()
+
+	// Process any remaining multiline value
+	if inMultiline {
+		if err := processMultilineValue(config, currentSection, currentKey, currentValue, lineNumber); err != nil {
+			errors = append(errors, err)
+		}
+	}
+
+	if len(errors) > 0 {
+		return errors
+	}
+
+	return nil
+}
+
+// parseFile reads and parses an INI file with support for include directives.
+func parseFile(filename string, config interface{}, delimiter string, includedFiles map[string]bool, depth int) []error {
+	if depth > 10 {
+		return []error{fmt.Errorf("maximum include depth exceeded")}
+	}
+
+	if includedFiles[filename] {
+		return []error{fmt.Errorf("circular include detected: %s", filename)}
+	}
+	includedFiles[filename] = true
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return []error{fmt.Errorf("failed to open file: %w", err)}
+	}
+	defer file.Close()
+
+	basePath := filepath.Dir(filename)
+	return parseReader(file, config, delimiter, includedFiles, depth+1, basePath)
+}
+
+// Parse parses the INI file content from an io.Reader and populates the config struct.
+func Parse(reader io.Reader, config interface{}) []error {
+	return parseReader(reader, config, "=", make(map[string]bool), 0, "")
+}
+
+// ParseWithDelimiter parses the INI file content from an io.Reader with a custom delimiter and populates the config struct.
+func ParseWithDelimiter(reader io.Reader, config interface{}, delimiter string) []error {
+	return parseReader(reader, config, delimiter, make(map[string]bool), 0, "")
 }
